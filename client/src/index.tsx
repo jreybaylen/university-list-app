c6191988efcb8be1212b2797f2267ee3cf8a86c4
John Rey M. Baylen <johnreybaylen211995@gmail.com>
2021-06-11 15:27:03 +0800

    Config for paths to shorten import url

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 
+import RootApp from '@pages/Root'
+
 ReactDOM.render(
 	<React.StrictMode>
-		Hello World!
+		<RootApp />
 	</React.StrictMode>,
 	document.getElementById('root')
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
---
+++ client/src/pages/Root.tsx
@@ -0,0 +1,11 @@
+function Root (): JSX.Element {
+    const rootElement = (
+        <div>
+            Hello World!
+        </div>
+    )
+
+    return rootElement
+}
+
+export default Root
