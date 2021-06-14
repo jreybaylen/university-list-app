775a0355e568eeb99fa037aab60c9081d6b14d9f
John Rey M. Baylen <johnreybaylen211995@gmail.com>
2021-06-15 02:13:08 +0800

    Card test has been added

---
+++ src/tests/components/card.test.tsx
@@ -0,0 +1,55 @@
+import { Fragment } from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { Card, Button } from '@components/index'
+
+describe('<Card /> Component', () => {
+    it('Should render without crashing', () => {
+        render(
+            <Card>
+                <p>Card component</p>
+            </Card>
+        )
+    })
+
+    it('Should display all the children component', () => {
+        const pContent = 'Card component'
+        const { getByText } = render(
+            <Card>
+                <p>{ pContent }</p>
+                <Button children="Click Me!" />
+            </Card>
+        )
+
+        expect(getByText(pContent)).toBeInTheDocument()
+        expect(getByText(/click me!/i)).toBeInTheDocument()
+    })
+
+    it('Should trigger "onClick" once the card has been clicked', () => {
+        const content = 'Card component'
+        const handleCardClick = jest.fn()
+        const { getByText } = render(
+            <Card onClick={ handleCardClick }>
+                <Fragment>{ content }</Fragment>
+            </Card>
+        )
+
+        fireEvent.click(getByText(content))
+        expect(handleCardClick).toHaveBeenCalled()
+    })
+
+    it('Should have an updated styles when onMouseEnter and onMouseLeave has been called', () => {
+        const content = 'Card component'
+        const { getByText } = render(
+            <Card>
+                <>{ content }</>
+            </Card>
+        )
+
+        expect(getByText(content)).toHaveStyle('box-shadow: none')
+        fireEvent.mouseOver(getByText(content))
+        expect(getByText(content)).toHaveStyle('box-shadow: rgb(0 0 0 / 20%) 0px 4px 8px 0px')
+        fireEvent.mouseLeave(getByText(content))
+        expect(getByText(content)).toHaveStyle('box-shadow: none')
+    })
+})
