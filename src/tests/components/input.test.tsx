b976c071250551d94156ba0e5700c1bbee1b7d0e
John Rey M. Baylen <johnreybaylen211995@gmail.com>
2021-06-15 03:32:41 +0800

    Input test has been added

---
+++ src/tests/components/input.test.tsx
@@ -0,0 +1,47 @@
+import { render, fireEvent } from '@testing-library/react'
+
+import { Input } from '@components/Input'
+
+describe('<Input /> Component', () => {
+    it('Should render without crashing', () => {
+        render(<Input />)
+    })
+    
+    it('Should render input when using the component', () => {
+        const { container } = render(<Input />)
+
+        expect(container.querySelector('input')).toBeInTheDocument()
+    })
+
+    it('Should have dynamic properties', () => {
+        const padding = '8px 10px'
+        const placeholder = "Testing"
+        const { getByDisplayValue } = render(
+            <Input
+                value={ 12 }
+                style={{ padding }}
+                placeholder={ placeholder }
+            />
+        )
+        const inputElement = getByDisplayValue(12)
+
+        expect(inputElement).toBeInTheDocument()
+        expect(inputElement).toHaveDisplayValue('12')
+        expect(inputElement).toHaveStyle(`padding: ${ padding }`)
+        expect(inputElement.getAttribute('placeholder')).toBe(placeholder)
+    })
+
+    it('Should trigger onChange when the component detected the value has been changed', () => {
+        const testId = 'input-element'
+        const handleInputChange = jest.fn()
+        const { getByLabelText } = render(
+            <Input onChange={ handleInputChange } aria-label={ testId } />
+        )
+        const inputElement = getByLabelText(testId)
+
+        expect(inputElement).toHaveDisplayValue('')
+        fireEvent.change(inputElement, { target: { value: 26 } })
+        expect(handleInputChange).toHaveBeenCalled()
+        expect(inputElement).toHaveDisplayValue('26')
+    })
+})
