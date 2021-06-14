d16dfe83be962b7d3e28b820a4379767a0ddbb53
John Rey M. Baylen <johnreybaylen211995@gmail.com>
2021-06-15 02:47:44 +0800

    Loader test has been added

---
+++ src/tests/components/loader.test.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react'
+
+import { Loader } from '@components/Loader'
+
+describe('<Loader /> Component', () => {
+    it('Should render without crashing', () => {
+        render(<Loader show={ true } />)
+    })
+
+    it('Should render component when "show" prop is "true"', () => {
+        const { getByText } = render(<Loader show={ true } />)
+
+        expect(getByText(/loading data.../i)).toBeInTheDocument()
+    })
+
+    it('Should not render component when "show" prop is "false"', () => {
+        const { queryByText } = render(<Loader show={ false } />)
+
+        expect(queryByText(/loading data.../i)).toBe(null)
+    })
+})
